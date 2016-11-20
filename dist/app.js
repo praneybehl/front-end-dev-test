fc01f4b632f194c8f9406a2eb40c22fd9218f922
Praney Behl <praneybehl@gmail.com>
2016-11-21 09:38:05 +1100

    Add gulp tasks for concatenating js files and test runner.

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,208 @@
+(function () {
+    var SELF;
+    var Utils = {
+
+
+        /*
+        * Gets MS calculations by different units.
+        * @returns {object} - Object containing different MS counts.
+        */
+        getMS: function() {
+            var oneSec = 1000
+		        ,oneMin = oneSec * 60
+		        ,oneHour = oneMin * 60
+		        ,oneDay = oneHour * 24
+		        ,oneYear = oneDay * 365;
+	
+            return {
+                oneSec: oneSec
+		        ,oneMin: oneMin
+		        ,oneHour: oneHour
+		        ,oneDay: oneDay
+                ,oneYear: oneYear
+            };
+        }
+
+        /*
+        * Gets different units from provided MS.
+        * @param {number} ms - Milliseconds used to calulate units.
+        * @returns {object} - Object containing different units.
+        */
+        , getUnitsFromMS: function( ms ) {
+            var secs = Math.round( ms / 1000 )
+                ,mins = Math.round( secs / 60 )
+                ,hours = Math.round( mins / 60 )
+                ,days = Math.round( hours / 24 );
+	
+            return {
+                secs: secs
+                ,mins: mins
+                ,hours: hours
+                ,days: days
+            };
+        }
+
+
+        /*
+        * Gets the day of the week in English.
+        * @param {number} utcTime/ind - UTC time value or array index. If this is ommitted, you will get the time right now and return today's day of the week name.
+        * @param {boolean} byInd - If true, will use `time` as an index value from the `days` array.
+        * @param {boolean} useShort - If true, will get shortened version of name.
+        * @returns {string} - Day of the week in English. Potentially add other languages here too.
+        */
+        , getWeekDay: function (time, byInd, useShort) {
+            var days = useShort ?
+                    ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat']
+                    :
+                    ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+            if (byInd === true) {
+                if (time >= 7) return null;
+                return days[time];
+            }
+            var date = time ? new Date(time) : new Date();
+            
+            return days[date.getDay()];
+        }
+
+        /*
+        * Gets the month of the year in English.
+        * @param {number} utcTime/ind - UTC time value or array index. If this is ommitted, you will get the time right now and return today's month of the year name.
+        * @param {boolean} byInd - If true, will use `time` as an index value from the `months` array.
+        * @param {boolean} useShort - If true, will get shortened version of name.
+        * @return {string} - Month of the year in English. Potentially add other languages here too.
+        */
+        , getMonthName: function (time, byInd, useShort) {
+            var months = useShort ?
+                    ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
+                    :
+                    ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+            if (byInd === true) {
+                if (time >= 12) return null;
+                return months[time];
+            }
+            var date = time ? new Date(time) : new Date();
+            return months[date.getMonth()];
+        }
+
+
+        /**
+         * Converts words in a string to pascal case.  
+         * @param {string[]} [lcWords] - Optionally provide an array `lcWords` that will get forced to lower case (or use default).
+         * @param {string[]} [UcWords] - Optionally provide an array `ucWords` that will not get modified (or use default).
+         * @returns {string} Modified string with replacements.
+         */
+		, toPascalCase: function (str, lcWords, ucWords) {
+		    
+            lcWords = lcWords || ["and", "a", "is", "not", "to", "&", "be", "was", "no", "are", "i", "for", "me", "you", "the", "in", "of", "out", "isn't"];
+		    ucWords = ucWords || ["NSW", "WA", "QLD", "TAS", "WA", "NT", "ACT", "SA", "VIC", "FAQ", "FAQs"];
+
+		    var wordCount = 0;
+		    return str.replace(/\w\S*/g, function (txt) {
+                // skip small words on first word
+		        if (wordCount > 0) {
+		            // make small words lower case
+		            for (var i = 0; i < lcWords.length; i++) {
+		                var lcTxt = txt.toLowerCase();
+		                if (lcTxt === lcWords[i]) {
+		                    return lcTxt;
+		                }
+		            }
+		        }
+		        // leave special words alone
+		        for (i = 0; i < ucWords.length; i++) {
+		            if (txt === ucWords[i]) return txt;
+		        }
+		        wordCount++;
+
+                // other words make Pascal case
+		        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+		    });
+		}
+
+
+
+        /**
+         * Gets a unique ID, based on a random number, with optional prefix.
+         * @param {string} [pref] - A string to prefix the ID with. For example 'topnav-'.
+         * @returns {string} Modified string with replacements.
+         */
+        , getUID: function(pref) {
+            return (pref || "") + Math.random().toString().replace(".", "");
+        }
+
+        
+        /**
+         * Replace between delimters for all instances in a string.
+         * @param {string} rxStart - the string to begin with.
+         * @param {string} rxEnd - the string to end with.
+         * @param {string} originalString - the string to search through.
+         * @param {string} replacementString - the string to put in between the delimeters.
+         * @param {boolean} [keepDelimeters] - whether to keep the delimeters in the returned value, or remove them.
+         * @param {boolean} [delimLineBreak] - whether to add a line break after each delimeter or not. Only applies if `keepDelimeteres` equals `true`.
+         * @returns {string} Modified string with replacements.
+         */
+        , replaceBetween: function(rxStart, rxEnd, originalString, replacementString, keepDelimeteres, delimLineBreak) {
+
+            var originalStart = rxStart;
+            var originalEnd = rxEnd;
+
+            // replace special characters that can cause problems in regex
+            if (rxStart.indexOf("?") != -1) rxStart = rxStart.split("?").join("\\?");
+            if (rxEnd.indexOf("?") != -1) rxEnd = rxEnd.split("?").join("\\?");
+
+            if (rxStart.indexOf("*") != -1) rxStart = rxStart.split("*").join("\\*");
+            if (rxEnd.indexOf("*") != -1) rxEnd = rxEnd.split("*").join("\\*");
+
+            var rx = new RegExp(rxStart + "[\\d\\D]*?" + rxEnd, "g");
+
+            // console.log( originalString.substr( rx ) );
+
+            if (!replacementString) replacementString = "";
+
+            var result;
+            if (keepDelimeteres === true) {
+                var lineBr = delimLineBreak ? "\n" : "";
+                result = originalString.replace(rx, originalStart + lineBr + replacementString + lineBr + originalEnd);
+            } else {
+                result = originalString.replace(rx, replacementString);
+            }
+
+            return result;
+        }
+
+        
+        /**
+         * Get the number of lines in a text element.
+         * @param {HTMLElement} el - The text element you're counting the lines from.
+         * @returns {number} Number of lines in the element.
+         */
+        , getTextLines: function (el) {
+            var thisPar = el.parentElement
+              , clone = thisPar.appendChild(el.cloneNode());
+
+            clone.innerHTML = "X";
+
+            var h = clone.offsetHeight;
+            thisPar.removeChild(clone);
+
+            var lines = Math.ceil(el.offsetHeight / h);
+
+            return lines;
+        }
+    }
+
+    // exposes library for browser and Node-based code (such as unit tests)
+    if (typeof window === "undefined") {
+        module.exports = Utils;
+    }
+    else {
+        window.utils = Utils;
+    }
+    
+    SELF = Utils;
+})();
 /*!
  * verge 1.9.1+201402130803
  * https://github.com/ryanve/verge
@@ -463,242 +668,54 @@
     }
 
 })();
-(function () {
-    var SELF;
-    var Utils = {
-
-
-        /*
-        * Gets MS calculations by different units.
-        * @returns {object} - Object containing different MS counts.
-        */
-        getMS: function() {
-            var oneSec = 1000
-		        ,oneMin = oneSec * 60
-		        ,oneHour = oneMin * 60
-		        ,oneDay = oneHour * 24
-		        ,oneYear = oneDay * 365;
-	
-            return {
-                oneSec: oneSec
-		        ,oneMin: oneMin
-		        ,oneHour: oneHour
-		        ,oneDay: oneDay
-                ,oneYear: oneYear
-            }
-        }
-
-        /*
-        * Gets different units from provided MS.
-        * @param {number} ms - Milliseconds used to calulate units.
-        * @returns {object} - Object containing different units.
-        */
-        , getUnitsFromMS: function( ms ) {
-            var secs = Math.round( ms / 1000 )
-                ,mins = Math.round( secs / 60 )
-                ,hours = Math.round( mins / 60 )
-                ,days = Math.round( hours / 24 );
-	
-            return {
-                secs: secs
-                ,mins: mins
-                ,hours: hours
-                ,days: days
-            }
-        }
-
-
-        /*
-        * Gets the day of the week in English.
-        * @param {number} utcTime/ind - UTC time value or array index. If this is ommitted, you will get the time right now and return today's day of the week name.
-        * @param {boolean} byInd - If true, will use `time` as an index value from the `days` array.
-        * @param {boolean} useShort - If true, will get shortened version of name.
-        * @returns {string} - Day of the week in English. Potentially add other languages here too.
-        */
-        , getWeekDay: function (time, byInd, useShort) {
-            var days = useShort ?
-                    ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat']
-                    :
-                    ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
-            if (byInd === true) {
-                if (time >= 7) return null;
-                return days[time];
-            }
-            var date = time ? new Date(time) : new Date();
-            
-            return days[date.getDay()];
-        }
-
-        /*
-        * Gets the month of the year in English.
-        * @param {number} utcTime/ind - UTC time value or array index. If this is ommitted, you will get the time right now and return today's month of the year name.
-        * @param {boolean} byInd - If true, will use `time` as an index value from the `months` array.
-        * @param {boolean} useShort - If true, will get shortened version of name.
-        * @return {string} - Month of the year in English. Potentially add other languages here too.
-        */
-        , getMonthName: function (time, byInd, useShort) {
-            var months = useShort ?
-                    ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
-                    :
-                    ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
-            if (byInd === true) {
-                if (time >= 12) return null;
-                return months[time];
-            }
-            var date = time ? new Date(time) : new Date();
-            return months[date.getMonth()];
-        }
-
-
-        /**
-         * Converts words in a string to pascal case.  
-         * @param {string[]} [lcWords] - Optionally provide an array `lcWords` that will get forced to lower case (or use default).
-         * @param {string[]} [UcWords] - Optionally provide an array `ucWords` that will not get modified (or use default).
-         * @returns {string} Modified string with replacements.
-         */
-		, toPascalCase: function (str, lcWords, ucWords) {
-		    
-            lcWords = lcWords || ["and", "a", "is", "not", "to", "&", "be", "was", "no", "are", "i", "for", "me", "you", "the", "in", "of", "out", "isn't"];
-		    ucWords = ucWords || ["NSW", "WA", "QLD", "TAS", "WA", "NT", "ACT", "SA", "VIC", "FAQ", "FAQs"];
-
-		    var wordCount = 0;
-		    return str.replace(/\w\S*/g, function (txt) {
-                // skip small words on first word
-		        if (wordCount > 0) {
-		            // make small words lower case
-		            for (var i = 0; i < lcWords.length; i++) {
-		                var lcTxt = txt.toLowerCase();
-		                if (lcTxt === lcWords[i]) {
-		                    return lcTxt;
-		                }
-		            }
-		        }
-		        // leave special words alone
-		        for (var i = 0; i < ucWords.length; i++) {
-		            if (txt === ucWords[i]) return txt;
-		        }
-		        wordCount++;
-
-                // other words make Pascal case
-		        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-		    });
-		}
-
-
-
-        /**
-         * Gets a unique ID, based on a random number, with optional prefix.
-         * @param {string} [pref] - A string to prefix the ID with. For example 'topnav-'.
-         * @returns {string} Modified string with replacements.
-         */
-        , getUID: function(pref) {
-            return (pref || "") + Math.random().toString().replace(".", "");
-        }
-
-        
-        /**
-         * Replace between delimters for all instances in a string.
-         * @param {string} rxStart - the string to begin with.
-         * @param {string} rxEnd - the string to end with.
-         * @param {string} originalString - the string to search through.
-         * @param {string} replacementString - the string to put in between the delimeters.
-         * @param {boolean} [keepDelimeters] - whether to keep the delimeters in the returned value, or remove them.
-         * @param {boolean} [delimLineBreak] - whether to add a line break after each delimeter or not. Only applies if `keepDelimeteres` equals `true`.
-         * @returns {string} Modified string with replacements.
-         */
-        , replaceBetween: function(rxStart, rxEnd, originalString, replacementString, keepDelimeteres, delimLineBreak) {
-
-            var originalStart = rxStart;
-            var originalEnd = rxEnd;
-
-            // replace special characters that can cause problems in regex
-            if (rxStart.indexOf("?") != -1) rxStart = rxStart.split("?").join("\\?");
-            if (rxEnd.indexOf("?") != -1) rxEnd = rxEnd.split("?").join("\\?");
-
-            if (rxStart.indexOf("*") != -1) rxStart = rxStart.split("*").join("\\*");
-            if (rxEnd.indexOf("*") != -1) rxEnd = rxEnd.split("*").join("\\*");
-
-            var rx = new RegExp(rxStart + "[\\d\\D]*?" + rxEnd, "g");
-
-            // console.log( originalString.substr( rx ) );
-
-            if (!replacementString) replacementString = "";
-
-            var result;
-            if (keepDelimeteres === true) {
-                var lineBr = delimLineBreak ? "\n" : "";
-                result = originalString.replace(rx, originalStart + lineBr + replacementString + lineBr + originalEnd);
-            } else {
-                result = originalString.replace(rx, replacementString);
-            }
-
-            return result;
-        }
-
-        
-        /**
-         * Get the number of lines in a text element.
-         * @param {HTMLElement} el - The text element you're counting the lines from.
-         * @returns {number} Number of lines in the element.
-         */
-        , getTextLines: function (el) {
-            var thisPar = el.parentElement
-              , clone = thisPar.appendChild(el.cloneNode());
-
-            clone.innerHTML = "X";
-
-            var h = clone.offsetHeight;
-            thisPar.removeChild(clone);
-
-            var lines = Math.ceil(el.offsetHeight / h);
-
-            return lines;
-        }
-
-
-    }
-
-    // exposes library for browser and Node-based code (such as unit tests)
-    if (typeof window === "undefined") {
-        module.exports = Utils;
-    }
-    else {
-        window.utils = Utils;
-    }
-    
-    SELF = Utils;
-})();
 (function(utils, evenNav, verge) {
 
+    // check for vibration support in browser
+    navigator.vibrate = navigator.vibrate ||
+        navigator.webkitVibrate ||
+        navigator.mozVibrate ||
+        navigator.msVibrate;
 
     function init() {
-        var el = document.querySelector(".topNav");
-        el.setAttribute("data-uid", utils.getUID()); // adds a unique id (uid) to the component
-        setTodayTitle(el);
-        setSubTitle(el);
-        setEvenNav(el);
+        var elems = document.querySelectorAll(".topNav");
+
+        for(var i=0; i<elems.length; i++){
+            var el = elems[i];
+            el.setAttribute("data-uid", utils.getUID()); // adds a unique id (uid) to the component
+            setTodayTitle(el);
+            setSubTitle(el);
+            setEvenNav(el);
+            onTapVibrate(el);
+        }
     }
 
 
     // Changes the upper case characters of '.topNav-subTitle' to pascal case
     function setSubTitle(el) {
-        // TODO: use 'utils.toPascalCase' to sanitize the contents of element '.topNav-subTitle'
+        var subTitleEl = el.querySelector(".topNav-subTitle");
+        subTitleEl.innerText = utils.toPascalCase(subTitleEl.innerText, ["has", "offices", "in", "and"], ["LEVO", "NSW", "VIC"]);
     }
 
 
     // Replaces '--WEEK_DAY_NAME--' and '--MONTH_NAME--' with dynamic contents in element '.topNav-title'
     function setTodayTitle(el) {
-        
-        // TODO: add today's week day and month name to the element '.topNav-title', using 'utils.getWeekDay' and 'utils.getMonthName'
+        var titleEl = el.querySelector(".topNav-title");
+        titleEl.innerText = titleEl.innerText.replace("--WEEK_DAY_NAME--", utils.getWeekDay())
+            .replace("--MONTH_NAME--", utils.getMonthName());
     }
 
 
-    // gets the window viewport width
+    // gets the window viewport width if its a mobile device
     function isMobileVP() {
-        return verge.viewportW() < 768
+        return verge.viewportW() < 768 ;
     }
+
+
+    // gets the window viewport width if its a tablet device
+    function isTabletVP(){
+        return verge.viewportW() >= 768 && verge.viewportW() < 992;
+    }
+
 
     // set the library 'EvenNav', which evenly sizes navigation elements horizontally
     function setEvenNav(el) {
@@ -709,11 +726,29 @@
 
         // needs a resize handler so it can clear mobile styles and recalculate when window is resized
         window.addEventListener("resize", function() {
-            var isMb = isMobileVP()
+            var isMb = isMobileVP();
 
             evenNav.clearClasses(el, !isMb);
             if(!isMb) evenNav.resize(el, !isMb);
         });
+    }
+
+    // add vibrate to every button if available
+    function onTapVibrate(el) {
+        var buttons = el.querySelectorAll(".nav-btn");
+
+        function vibrateDevice(){
+            if (navigator.vibrate) {
+                console.log('vibration supported');
+                navigator.vibrate(500);
+            }
+            else {
+                console.log('vibration not supported');
+            }
+        }
+        for(var i=0; i<buttons.length; i++){
+            buttons[i].addEventListener("touchstart", vibrateDevice);
+        }
     }
 
 
--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,11 @@ const stylish = require('jshint-stylish');
 const handlebars = require('gulp-compile-handlebars');
 const rename = require('gulp-rename');
 const webserver = require('gulp-webserver');
+const concat = require('gulp-concat');
+const mocha = require("gulp-mocha");
+const gUtil = require("gulp-util");
 
- 
+
 gulp.task('sass', function () {
   return gulp.src('./app/**/*.scss')
     .pipe(sass().on('error', sass.logError))
@@ -21,14 +24,26 @@ gulp.task('sass', function () {
 });
  
 gulp.task('watch', function () {
-  gulp.watch('./app/**/*.scss', ['sass']);
-  gulp.watch('./app/**/*.handlebars', ['hbs']);
-  gulp.watch('./app/**/*.js', ['js']);
+    //run test the first time it starts watching
+    gulp.run('test');
+    gulp.watch('./app/**/*.scss', ['sass']);
+    gulp.watch('./app/**/*.handlebars', ['hbs']);
+    gulp.watch('./app/**/*.js', ['js']);
+    // watch tests and run test when test change
+    gulp.watch('./test/*.js', ['test']);
 });
 
 
 gulp.task('default', ['lint', 'sass', 'hbs', 'js'], function() {
-    // Add tests here
+    // run tests
+    gulp.run('test');
+});
+
+// added task to run mocha tests
+gulp.task('test', function(){
+    return gulp.src(["test/*.js"], {read:false})
+        .pipe(mocha({reporter: 'list'}))
+        .on('error',gUtil.log);
 });
 
 gulp.task('lint', function() {
@@ -40,17 +55,29 @@ gulp.task('lint', function() {
 });
 
 gulp.task('hbs', function () {
+    // Sample menu items passed as data to the TopNav components
     var templateData = {
-        
+        theme1MenuItems: ["Home","Explore","Plan","Get in touch","Sponsorship","Events"],
+        theme2MenuItems: ["Home","Explore","Plan","A Long Title That Pushes 2 lines","Sponsorship","Events"]
     },
     options = {
         batch : ['./app/TopNav/hbs/']
     };
- 
+
     return gulp.src('app/Global/hbs/TopNavExample.handlebars')
         .pipe(handlebars(templateData, options))
         .pipe(rename('top-nav-example.html'))
         .pipe(gulp.dest('dist'));
+});
+
+// added task to combine all js files in load order and save them as app.js in dist
+gulp.task('js', function() {
+    return gulp.src(["./app/Global/js/Utils.js",
+        "./node_modules/verge/verge.js",
+        "./libs/evennav/evennav.js",
+        "./app/TopNav/js/TopNav.js"])
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('webserver', function() {
@@ -65,5 +92,3 @@ gulp.task('webserver', function() {
       port: 4000
     }));
 });
-
-// TODO: combine JS files into single file 'app.js'. See TopNav.js for dependency list
